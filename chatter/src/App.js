675400e8ee91b36986b34e846dc05940cbbef05c
Yudian Xu <yudianxu@Yudians-MacBook-Pro.local>
2022-01-23 19:25:37 -0800

    first react

---
+++ chatter/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo" />
+        <span className="title">LET'S CHAT!</span>
+      </header>
+      <footer className="footer">
+        <input className="text-input" />
+        <button className="send">↑</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
