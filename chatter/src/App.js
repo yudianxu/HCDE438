5bb84acccd1c2dd16f5e0c80c7653d1c9db7781a
Yudian Xu <yudianxu@Yudians-MacBook-Pro.local>
2022-02-04 20:29:26 -0800

    camera added

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -1,32 +1,68 @@
 import "./App.css";
 import TextInput from "./TextInput";
-import { useState } from "react";
+import React, { useState } from "react";
 import Message from "./Message";
+import { use100vh } from "react-div-100vh";
+import Camera from 'react-snap-pic'
 
+
+// this is a Component call App
 function App() {
-  const [messages, setMessages] = useState([]);
+  const height = use100vh();
+  const [showCamera, setShowCamera] = useState(false)
+  // useState creats a magic variable
+  // here its called "messages"
+  // the initial value is an empty array []
+  // "setMessages" is a function that is used to update "messages"
+  let [messages, setMessages] = useState([]);
+
+  // "sendMessage" runs whenver we click the send button
   function sendMessage(text) {
+    if (!text) return;
+    // we'll create a new message object
     const newMessage = {
-      text,
+      text: text,
       time: Date.now(),
       user: "Evan",
     };
+    // set the "messages" to be a new array
+    // that contains the new message + all the old messages
     setMessages([newMessage, ...messages]);
+    let takePicture = (img) => {
+      console.log(img)
+      setShowCamera(false)
+    };
   }
+   
+  // every time state changes, React "re-renders"
+  // so this console.log will run again
   console.log(messages);
+
+  // we return the HTML
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ height: height, minHeight: height, maxHeight: height }}
+    >
+      {showCamera && <Camera takePicture={this.takePicture} />}
+
       <header className="header">
         <div className="logo" />
         <span className="title">CHATTER!</span>
       </header>
       <div className="messages">
-        {messages.map((msg) => {
-          return <Message {...msg} />;
+        {messages.map((msg, i) => {
+          // loop over every message in the "messages" array
+          // and return a Message component
+          // we are "spreading" all the items in "msg" into the props
+          // "key" needs to be a unique value for each item
+          return <Message {...msg} key={i} />;
         })}
       </div>
-      <TextInput sendMessage={sendMessage} />
-    </div>
+      {/* the sendMessage prop on TextInput = the sendMessage function */}
+      <TextInput sendMessage={sendMessage} 
+     showCamera={()=>setShowCamera(true)}
+/>    </div>
   );
 }
 
--- chatter/src/TextInput.js
+++ chatter/src/TextInput.js
@@ -1,5 +1,6 @@
 import "./TextInput.css";
 import { useState } from "react";
+import { FiSend, FiCamera } from 'react-icons/fi'
 
 function TextInput(props) {
   const [text, setText] = useState("");
@@ -16,6 +17,10 @@ function TextInput(props) {
 
   return (
     <footer className="footer">
+        <button onClick={props.showCamera}
+    style={{left:10, right:'auto'}}>
+    <FiCamera style={{height:15, width:15}} />
+</button>
       <input
         className="text-input"
         value={text}
@@ -23,7 +28,7 @@ function TextInput(props) {
         onKeyPress={onKeyPress}
       />
       <button className="send" onClick={send}>
-        ↑
+        <FiSend />
       </button>
     </footer>
   );
