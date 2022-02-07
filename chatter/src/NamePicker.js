3c429a247f0b5959d18b41713e9d4c41c70b9c25
Yudian Xu <yudianxu@Yudians-MacBook-Pro.local>
2022-02-06 22:25:25 -0800

    component added

--- chatter/src/NamePicker.js
+++ chatter/src/NamePicker.js
@@ -1,28 +1,41 @@
 import "./NamePicker.css";
 import { useState } from "react";
 import { BsPencilSquare } from 'react-icons/bs';
+import { AiOutlineCheckSquare } from 'react-icons/ai';
 
 function NamePicker(props) {
-    const [editName, setEditName] = useState(false);
-    function set() {
-        props.setEditName(editName);
-        setName("");
-      }
-    const [name, setName] = useState('')
- 
+  const [editName, setEditName] = useState(false);
+  function set() {
+    props.setEditName(editName);
+    setName("");
+  }
+  const [name, setName] = useState('')
 
-return (
-    {editName &&       
-     <button className="name"
-     onClick={props.editName}
-            style={{left:10, right:'auto'}}>
-            <BsPencilSquare style={{height:15, width:15}} />
-        </button>}
+
+  return (<>
+    {editName ? <div
+      style={{ flex: 1,display:"flex",alignItems:"center",justifyContent:"flex-end"}}>
       <input
         className="name-input"
+        value={name}
         onChange={(e) => setName(e.target.value)}
       />
+        <AiOutlineCheckSquare onClick={() => {
+          setEditName(false);
+        }} style={{ height: 30, width: 30, color: "#fff",marginLeft: "5px"}} />
+      
+    </div> :
+      (<div style={{ flex: 1,display:"flex",alignItems:"center",justifyContent:"flex-end"}}>
+        <span style={{ color: "#fff" }}>
+          {name ? name : "Set Username"}
+        </span>
+        <BsPencilSquare style={{ height: 15, width: 15, color: "#fff", cursor: "pointer", marginLeft: "5px" }}
+          onClick={() => {
+            setEditName(true);
+          }} /></div>)
+    }
+  </>
   );
 }
 
-  export default NamePicker;
+export default NamePicker;
